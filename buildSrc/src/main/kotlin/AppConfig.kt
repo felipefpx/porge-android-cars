126ec922f4b154daf2039e1fc6ad1093228f8c4e
Felipe Porge Xavier <felipefpx@gmail.com>
2020-12-26 11:35:34 -0300

    Increase app version and target android api

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,10 +1,10 @@
 object AppConfig {
     const val APPLICATION_ID = "me.porge.android.cars"
 
-    const val VERSION_CODE = 3
+    const val VERSION_CODE = 4
     const val VERSION_NAME = "1.1.0"
 
-    const val TARGET_ANDROID_API = 28
+    const val TARGET_ANDROID_API = 29
     const val MIN_ANDROID_API = 24
 
     const val TEST_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
